6111750533f83b0136b4e4425347cd370d0e1b0d
Venkatakrishnan R <venkrish007@gmail.com>
2020-07-28 21:02:40 -0700

    Added close downloaded tabs feature

--- background.js
+++ background.js
@@ -1,3 +1,21 @@
+tabs = []
+urls = []
+
+var button;
+
+function close(tabs) {
+	if(tabs.length > 0) {
+		console.log("Close tabs called");
+		for(i in tabs) {
+			chrome.tabs.remove(tabs[i]);
+		}
+		button.style.display = "none";
+	}
+	else {
+		console.log("Nothing to close");
+	}
+}
+
 function download(urls) {
 	if(urls.length > 0) {
 		for(i in urls) {
@@ -7,6 +25,7 @@ function download(urls) {
 			});
 		}
 		window.document.getElementById("status").innerHTML='Download complete';
+		button.style.display = "";
 	}
 	else {
 		console.log("Nothing to download");
@@ -15,7 +34,6 @@ function download(urls) {
 }
 
 chrome.windows.getAll({populate:true},function(windows){
-	urls = []
 	windows.forEach(function(window){
 		window.tabs.forEach(function(tab){
 			if(tab.url.includes('gfycat.com')) {
@@ -30,6 +48,7 @@ chrome.windows.getAll({populate:true},function(windows){
 				json = JSON.parse(request.responseText);
 				link = json.gfyItem.mp4Url;
 				urls.push(link);
+				tabs.push(tab.id);
 			}
 			else if (tab.url.includes('redgifs.com')) {
 				name = tab.url.substring(tab.url.lastIndexOf("/") + 1);
@@ -43,26 +62,22 @@ chrome.windows.getAll({populate:true},function(windows){
 				json = JSON.parse(request.responseText);
 				link = json.gfyItem.mp4Url;
 				urls.push(link);
-			}
-			else if (tab.url.includes('gifdeliverynetwork.com')) {
-				name = tab.url.substring(tab.url.lastIndexOf("/") + 1);
-				name = name.split('.')[0];
-				name = name.split('-')[0];
-				jsonendpoint = 'https://api.gifdeliverynetwork.com/v1/gfycats/' + name
-				console.log(jsonendpoint);
-				request = new XMLHttpRequest();
-				request.open("GET", jsonendpoint, false);
-				request.send(null);
-				json = JSON.parse(request.responseText);
-				link = json.gfyItem.mp4Url;
-				urls.push(link);
+				tabs.push(tab.id);
 			}
 			else if(tab.url.includes('imgur.com') && tab.url.includes('gifv')) {
 				link = tab.url.replace('gifv', 'mp4');
 				urls.push(link);
+				tabs.push(tab.id);
 			}
 		});
 	});
-	console.log("calling download");
+	console.log("Calling Download");
+	button = window.document.getElementById("btn");
+
+	button.addEventListener("click", function(){
+		console.log("Clicked Close Tabs");
+		close(tabs);
+	});
+
 	download(urls);
 });
